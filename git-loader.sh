c9ed1d336a79074f4bdcf85d32a845b2036f5792
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2012-12-11 16:40:59 +0400

    Main Script file

---
+++ git-loader.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Shell Script By Ziyaddin Sadigov (C)
+
+#ask for typing github project author and project name
+echo "Type your GitHub project url for download"
+
+#colect information
+read url
+
+#get data from github project page
+wget "https://github.com/$url/archive/master.zip"
+
+if [ "$?" = "0" ]; then
+
+	#and finally tell about success
+	echo "Successfully Downloaded"
+
+else
+
+	echo "Download Failed"
+
+fi
